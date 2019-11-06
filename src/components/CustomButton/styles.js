d58b612679469519c5f12cd951fb67f37be0c566
arllain <arllain@gmail.com>
2019-11-06 11:50:37 -0300

    Made some changes in CustomButton Component

--- src/components/CustomButton/styles.js
+++ src/components/CustomButton/styles.js
@@ -3,15 +3,15 @@ import styled from 'styled-components/native';
 export const Container = styled.TouchableOpacity`
    top: ${props => props.top};
    width: ${props => props.width};
-   height: ${props => props.height}px;
-   padding: ${props => props.padding}px;
-   border-radius: ${props => props.radius}px;
+   height: ${props => `${props.height}px`};
+   padding: ${props => `${props.padding}px`};
+   border-radius: ${props => `${props.radius}px`};
    background-color: ${props => props.background};
 `;
 
 export const TextButton = styled.Text`
    color: ${props => props.color};
-   font-size: ${props => props.fontSize}px;
+   font-size: ${props => `${props.fontSize}px`};
    font-weight: ${props => props.fontWeight};
    text-align: ${props => props.textAlign};
 `;
