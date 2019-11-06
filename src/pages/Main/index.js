edcd8a0ca9ba6354dcde005534d993708ec44df1
arllain <arllain@gmail.com>
2019-11-06 10:41:23 -0300

    Made some changes in CustomButton Component

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -10,22 +10,33 @@ export default function Main() {
          <CustomButton
             top="300px"
             width="200"
-            height="40px"
-            padding="10px"
-            radius="20px"
+            height="40"
+            padding="10"
+            radius="20"
             color="#FFFFFF"
             text="Autenticar"
+            textAlign="center"
             background="#3734eb"
+            fontWeight="bold"
+            fontSize="16"
          ></CustomButton>
          <CustomButton
             top="320px"
             width="150"
-            height="40px"
-            padding="10px"
-            radius="20px"
+            height="40"
+            padding="10"
+            radius="20"
             color="#3734eb"
             text="Sair do aplicativo"
+            textAlign="center"
             background="#FFFFFF"
+            fontWeight="bold"
+            fontSize="16"
+            onPress={() =>
+               alert(
+                  'Ao encerrar a sessão, todos os seus dados não sincronizados serão excluídos de seu dispositivo. Deseja realmente encerrar sua sessão neste dispositivo?'
+               )
+            }
          ></CustomButton>
       </Container>
    );
